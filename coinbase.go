5e79ff5c0c9b20713a10c6af92e0410f2650c28c
denkhaus <peristaltic@gmx.net>
2014-03-19 18:50:46 +0000

    make progress

--- coinbase.go
+++ coinbase.go
@@ -2,54 +2,96 @@ package main
 
 import (
 	"bitbucket.org/mendsley/tcgl/applog"
+  "github.com/grantmd/go-coinbase"
 	"fmt"
-	"github.com/coreos/go-etcd/etcd"
-	"github.com/stretchr/objx"
+	"github.com/coreos/go-etcd/etcd"	
 	"strconv"
+  "strings"
 )
+
 
 type CoinbaseProvider struct {
 	etcdClient *etcd.Client
+  coinbaseClient * coinbase.Client
 }
 
 func (p *CoinbaseProvider) Init(etcdClient *etcd.Client) error {
 	applog.Infof("initialize coinbase provider")
-
-	p.etcdClient = etcdClient
+  
+	p.coinbaseClient = &coinbase.Client{APIKey: ""}
+  p.etcdClient = etcdClient    
+  
 	return p.maintainCurrencyMap()
 }
 
 func (p *CoinbaseProvider) CollectData() error {
 
+  data, err := p.coinbaseClient.CurrenciesExchangeRates()
+  
+  if err != nil {
+		return err
+	}
+  
+  rates := data.(map[string]string)
+  if rates != nil && len(rates) != 0 {
+		
+    ts := time.Now.Unix()
+		for symbol, price := range rates {			
+      
+      marketid, err  := p.getMarketIdBySymbol(symbol)
+      
+      if err != nil {
+		    return err
+	    }   
+      
+      path := fmt.Sprintf("/mkt/%s/quotes/%d/%s", COINBASE_PATH_ID, ts, marketid)			
+			p.etcdClient.Set(fmt.Printf("%s/p", path), price)			
+    }
+  }
+  
 	return nil
 }
 
+func (p *CoinbaseProvider) extractSymbolCodes(symbol string) []string{
+  
+  if symbol != nil && len(symbol) > 0{
+      symbol = strings.ToUpper()         
+      return strings.Replace(symbol, "_TO_")      
+  }
+  
+  return nil
+}
+
+func (p *CoinbaseProvider) getMarketIdBySymbol(symbol string) (string, error){
+  
+  codes = p.extractSymbolCodes(symbol)
+  
+  if codes != nil && len(codes) != 2 {
+    applog.Errorf("could not extract symbol codes:: symbol %s", symbol)
+  }  
+}
+
+
+
 func (p *CoinbaseProvider) maintainCurrencyMap() error {
 
-	data, err := FetchData(COINBASE_API_CURRENCIES_URL)
-
+  applog.Debugf("initialize coinbase provider")
+  data, err := p.coinbaseClient.Currencies()
+	
 	if err != nil {
 		return err
 	}
 
-	if data != nil && len(data) != 0 {
-
-		jdata := fmt.Sprintf("{ \"data\": %s}", string(data))
-		m, err := objx.FromJSON(jdata)
-
-		if err != nil {
-			return err
-		}
-
-		k := m.Get("data").Data()
-		for symid, symdata := range k.([]interface{}) {
-			f := symdata.([]interface{})
-			path := fmt.Sprintf("/mkt/cnbase/map/%s", f[1])
+  currencies := data.([][]string)
+	if currencies != nil && len(currencies) != 0 {
+		
+		for symid, symdata := range currencies {			
+			path := fmt.Sprintf("/mkt/cnbase/map/%s", symdata[1])
 
 			//etcdClient.Get(path, false, false)
 
 			p.etcdClient.Set(fmt.Sprintf("%s/id", path), strconv.Itoa(symid), 0)
-			p.etcdClient.Set(fmt.Sprintf("%s/name", path), f[0].(string), 0)
+			p.etcdClient.Set(fmt.Sprintf("%s/name", path), symdata[0], 0)
 		}
 	}
 
@@ -57,10 +99,10 @@ func (p *CoinbaseProvider) maintainCurrencyMap() error {
 }
 
 // create new coinbase provider.
-func NewCoinbaseProvider() *CoinbaseProvider {
+func newCoinbaseProvider() Provider {
 	return &CoinbaseProvider{}
 }
 
 func init() {
-	RegisterProvider("coinbase", NewCoinbaseProvider())
+	RegisterProvider("coinbase", newCoinbaseProvider())
 }
--- const.go
+++ const.go
@@ -3,6 +3,6 @@ package main
 
 const(
   CRYPTSY_API_URL = "http://pubapi.cryptsy.com/api.php?method=marketdatav2"
-  COINBASE_API_CURRENCIES_URL = "https://coinbase.com/api/v1/currencies"
-  COINBASE_API_RATES_URL = "https://coinbase.com/api/v1/currencies/exchange_rates"
+  CRYPTSY_PATH_ID = "crtsy"  
+  COINBASE_PATH_ID = "cnbase"
 )
